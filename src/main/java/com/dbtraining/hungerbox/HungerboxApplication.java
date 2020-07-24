18b11cfbbb6835a91af1bf9623ecf22337e20be8
Venkanna Godishala <gvs086@gmail.com>
2020-07-24 13:15:37 +0530

    initial commit

---
+++ src/main/java/com/dbtraining/hungerbox/HungerboxApplication.java
@@ -0,0 +1,13 @@
+package com.dbtraining.hungerbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungerboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungerboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbtraining/hungerbox/HungerboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbtraining.hungerbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungerboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
